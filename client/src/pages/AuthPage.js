a7a2ef4c537afed1dd4a3c9d74a853651dd10695
svasyanovych <svitlana.v92@gmail.com>
2020-04-27 00:39:59 +0300

    added basic css and html for auth page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>AuthPage</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Short Links</h1>
+                <div className="card purple lighten-3 z-depth-5">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Enter your email"
+                                    id="email"
+                                    type="text"
+                                    name="email"
+                                    className="purple-input"
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input
+                                    placeholder="Enter your password"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    className="purple-input"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn yellow darken-4" style={{ marginRight: 10 }}>
+                            Log in
+                        </button>
+                        <button className="btn grey lighten-1 black-text">Sign up</button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
